daad42bbd8debff74d2e4e3da73534a45aa25d04
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-18 12:38:20 +0330

    Add project files.

---
+++ src/Portal/Persistance/PortalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Portal.Persistance
+{
+    public class PortalDbContext : IdentityDbContext
+    {
+        public PortalDbContext(DbContextOptions<PortalDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
