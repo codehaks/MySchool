c3d2066ea2941d78303148e21288bfd88841729d
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-18 12:42:04 +0330

    Models

---
+++ src/Portal/Domain/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Portal.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string GivenName { get; set; }
+        public string SureName { get; set; }
+        public int Age { get; set; }
+        public float GPA { get; set; }
+
+    }
+}
